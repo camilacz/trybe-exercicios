fa31cbd9701fbdb5bf1935e1c2ec977539c79641
camila <camilaczeg@gmail.com>
2021-10-08 18:27:36 -0300

    questoes 2 a 4

--- m1-fundamentos/b4-intro-javascript-e-logica-de-programacao/d1-javascript-primeiros-passos/scripts.js
+++ m1-fundamentos/b4-intro-javascript-e-logica-de-programacao/d1-javascript-primeiros-passos/scripts.js
@@ -8,3 +8,31 @@ console.log("Multiplicação: " + (a * b));
 console.log("Divisão: " + (a / b));
 console.log("Módulo: " + (a % b));
 
+//atividade 2
+if (a > b) {
+  console.log("'a' é maior do que 'b'");
+} else {
+  console.log("'b' é maior do que 'a'");
+}
+
+//atividade 3
+const c = 10;
+
+if (a > b && a > c) {
+  console.log("'a' é o maior número: " + a);
+} else if (b > a && b > c) {
+  console.log("'b' é o maior número: " + b);
+} else {
+  console.log("'c' é o maior número: " + c);
+}
+
+//atividade 4
+const number = -17;
+
+if (number > 0) {
+  console.log("positive");
+} else if (number < 0) {
+  console.log("negative");
+} else {
+  console.log("zero");
+}
