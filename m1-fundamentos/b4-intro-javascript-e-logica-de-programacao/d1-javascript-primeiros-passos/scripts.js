0d99fc8c53a0d7e342c5fa6473640a5046256bf5
camila <camilaczeg@gmail.com>
2021-10-08 21:13:19 -0300

    resolvi a questao 7

--- m1-fundamentos/b4-intro-javascript-e-logica-de-programacao/d1-javascript-primeiros-passos/scripts.js
+++ m1-fundamentos/b4-intro-javascript-e-logica-de-programacao/d1-javascript-primeiros-passos/scripts.js
@@ -36,3 +36,42 @@ if (number > 0) {
 } else {
   console.log("zero");
 }
+
+//atividade 5
+const angleA = 28;
+const angleB = 60;
+const angleC = 92;
+
+if (angleA > 0 && angleB > 0 && angleC > 0) {
+  if (angleA + angleB + angleC === 180) {
+    console.log(true);
+  } else {
+    console.log(false);
+  }
+} else {
+  console.log("Erro: âmgulo inválido");
+}
+
+//atividade 6
+
+//atividade 7
+const grade = 82;
+
+
+if (grade >= 0 && grade <= 100) {
+  if (grade >= 90) {
+    console.log("A");
+  } else if (grade >= 80) {
+    console.log("B");
+  } else if (grade >= 70) {
+    console.log("C");
+  } else if (grade >= 60) {
+    console.log("D");
+  } else if (grade >= 50) {
+    console.log("E");
+  } else if (grade < 50) {
+    console.log("F")
+  }
+} else {
+  console.log("Error: unexpected value in 'grade'");
+}
