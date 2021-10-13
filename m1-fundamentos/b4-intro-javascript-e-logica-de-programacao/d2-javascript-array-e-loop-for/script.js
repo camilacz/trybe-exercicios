25ff4cf66106b9b3fb86bb74dd05d5c97afda352
camila <camilaczeg@gmail.com>
2021-10-13 14:29:52 -0300

    Exercicios 1 e 2

---
+++ m1-fundamentos/b4-intro-javascript-e-logica-de-programacao/d2-javascript-array-e-loop-for/script.js
@@ -0,0 +1,15 @@
+let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+
+//Exercício 1 - imprima todos os valores da array
+for (number of numbers) {
+  console.log(number);
+}
+
+//Exercício 2 - some todos os valores da array
+let soma = 0;
+
+for (let i = 0; i < numbers.length; i += 1) {
+  soma += numbers[i];
+}
+
+console.log(soma)
