fc5b2d4cb07062dda114f84dd1775d56942e31a8
camila <camilaczeg@gmail.com>
2021-11-12 11:54:36 -0300

    Parte 2 - exercicio 1

---
+++ m1-fundamentos/b7-intro-a-es6-e-testes-unitarios/d2-js-es6-objects/objects2.js
@@ -0,0 +1,24 @@
+const lesson1 = {
+  materia: 'Matemática',
+  numeroEstudantes: 20,
+  professor: 'Maria Clara',
+  turno: 'manhã',
+};
+
+const lesson2 = {
+  materia: 'História',
+  numeroEstudantes: 20,
+  professor: 'Carlos',
+};
+
+const lesson3 = {
+  materia: 'Matemática',
+  numeroEstudantes: 10,
+  professor: 'Maria Clara',
+  turno: 'noite',
+};
+
+// Exercício 1
+const newKey = (object, newKey, value) => object[newKey] = value;
+
+newKey(lesson2, 'turno', 'noite');
