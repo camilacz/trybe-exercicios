68d073dd24df4dc92f83c049d9096575c398d7a4
camila <camilaczeg@gmail.com>
2021-11-23 14:10:37 -0300

    Exercicio 1

---
+++ m1-fundamentos/b8-higher-order-functions-js-es6/d4-js-hofs-reduce/exercise1.js
@@ -0,0 +1,14 @@
+// EXERCÍCIO: Dada uma matriz, transforme em um array.
+
+const arrays = [
+  ['1', '2', '3'],
+  [true],
+  [4, 5, 6],
+];
+
+function flatten() {
+  // escreva seu código aqui
+  return arrays.reduce((result, array) => result.concat(array));
+}
+
+console.log(flatten());
