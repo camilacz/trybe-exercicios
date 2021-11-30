31d2848d12af824de697feebb016e4d88b861b86
camila <camilaczeg@gmail.com>
2021-11-30 14:15:55 -0300

    Exercicio 3

---
+++ m1-fundamentos/b9-js-e-testes-assincronos/d1-js-assincronos-e-callbacks/exercise3.js
@@ -0,0 +1,14 @@
+// EXERCÍCIO 3 - Modifique getPlanet de forma que Marte seja impresso assincronamente, depois de 4 segundos.
+
+const getPlanet = () => {
+  const mars = {
+    name: "Mars",
+    distanceFromSun: {
+      value: 227900000,
+      measurementUnit: "kilometers",
+    },
+  };
+  setTimeout(() => console.log("Returned planet: ", mars), 4000);
+};
+
+getPlanet(); // imprime Marte depois de 4 segundos
