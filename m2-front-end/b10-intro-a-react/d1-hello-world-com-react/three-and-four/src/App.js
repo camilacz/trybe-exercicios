99f95bb6c5b884bd44a0501bb077ae190f5f9077
camila <camilaczeg@gmail.com>
2021-12-11 18:53:31 -0300

    Exercicio 3 e bagunça com o css >.<

---
+++ m2-front-end/b10-intro-a-react/d1-hello-world-com-react/three-and-four/src/App.js
@@ -0,0 +1,23 @@
+import React from 'react';
+
+import Header from './Header';
+import Content from './Content';
+import Footer from './Footer';
+
+import './App.css';
+
+class App extends React.Component {
+  render() {
+    return (
+      <>
+        <Header />
+        <main className="main">
+          <Content />
+          <Footer />
+        </main>
+      </>
+    );
+  }
+}
+
+export default App;
