9bb7fce8a2ed7c0556a9d3c79d41a4988a6d3926
camila <camilaczeg@gmail.com>
2022-05-03 11:40:25 -0300

    22.2 - exercicio 1

---
+++ m3-back-end/b22-intro-desenvolvimento-web-com-nodejs/d2-nodejs-fluxo-assincrono/README.md
@@ -0,0 +1,11 @@
+# **Bloco 22 -** Introdução ao desenvolvimento Web com Node.js
+
+## DIA 2 - Fluxo Assíncrono
+
+&nbsp;
+
+### **Exercícios 1:** Crie uma função que receba três parâmetros e retorna uma `Promise`.
+  * Caso algum dos parâmetros recebidos não seja um número, rejeite a Promise com o motivo `"Informe apenas números"`.
+  * Caso todos os parâmetros sejam númericos, some os dois primeiros e multiplique o resultado pelo terceiro (`(a + b) * c`).
+  * Caso o resultado seja menor que 50, rejeite a Promise com o motivo `"Valor muito baixo"`.
+  * Caso o resultado seja maior que 50, resolva a Promise com o valor obtido.
--- m3-back-end/b22-intro-desenvolvimento-web-com-nodejs/d2-nodejs-fluxo-assincrono/index.js
+++ m3-back-end/b22-intro-desenvolvimento-web-com-nodejs/d2-nodejs-fluxo-assincrono/index.js
@@ -1,1 +1,32 @@
-console.log('hi');
+const mathThis = (a, b, c) => {
+  // yes, math is a verb now o.O
+
+  return new Promise((resolve, reject) => {
+
+    const params = [a, b, c];
+    if (params.some((param) => typeof param !== 'number')) {
+      return reject(new Error('Informe apenas números'));
+    }
+
+    const result = (a + b) * c;
+
+    if (result < 50) return reject(new Error('Valor muito baixo'));
+
+    resolve(result);
+  });
+};
+
+// Todos números
+mathThis(5, 5, 10)
+  .then((result) => console.log(result))
+  .catch((err) => console.log(err.message));
+
+// Valor < 50
+mathThis(1, 1, 10)
+  .then((result) => console.log(result))
+  .catch((err) => console.log(err.message));
+
+// Nem todos são números
+mathThis(5, 2, 'potato')
+  .then((result) => console.log(result))
+  .catch((err) => console.log(err.message));
