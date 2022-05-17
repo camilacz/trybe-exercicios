658bee8ce43c17a8845ce778102d5475ef753c42
camila <camilaczeg@gmail.com>
2022-05-17 20:13:13 -0300

    feat: adiciona rota GET /books/:id

--- m3-back-end/b24-nodejs-orm-autenticacao/d1-orm-interface-da-aplicacao-com-db/routes/bookRoute.js
+++ m3-back-end/b24-nodejs-orm-autenticacao/d1-orm-interface-da-aplicacao-com-db/routes/bookRoute.js
@@ -4,5 +4,6 @@ const bookController = require('../controllers/bookController');
 const bookRouter = express.Router();
 
 bookRouter.get('/', bookController.getAll);
+bookRouter.get('/:id', bookController.getById);
 
 module.exports = bookRouter;
