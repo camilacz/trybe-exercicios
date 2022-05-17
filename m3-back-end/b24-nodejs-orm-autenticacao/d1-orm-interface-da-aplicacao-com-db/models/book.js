4d19f9645ad594decf34012e7734a519cb3e6cf7
camila <camilaczeg@gmail.com>
2022-05-17 19:21:59 -0300

    feat: add Book model

---
+++ m3-back-end/b24-nodejs-orm-autenticacao/d1-orm-interface-da-aplicacao-com-db/models/book.js
@@ -0,0 +1,10 @@
+const Book = (Sequelize, DataTypes) =>
+  Sequelize.define('Book', {
+    title: DataTypes.STRING,
+    author: DataTypes.STRING,
+    pageQuantity: DataTypes.INTEGER,
+    createdAt: DataTypes.DATE,
+    updatedAt: DataTypes.DATE,
+  });
+
+module.exports = Book;
