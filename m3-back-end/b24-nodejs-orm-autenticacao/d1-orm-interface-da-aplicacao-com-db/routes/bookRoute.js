ed165b6c7701d3ebd07831cb559ab34553749dd5
camila <camilaczeg@gmail.com>
2022-05-17 21:29:14 -0300

    feat: adiciona rota PUT /books/:id

--- m3-back-end/b24-nodejs-orm-autenticacao/d1-orm-interface-da-aplicacao-com-db/routes/bookRoute.js
+++ m3-back-end/b24-nodejs-orm-autenticacao/d1-orm-interface-da-aplicacao-com-db/routes/bookRoute.js
@@ -6,5 +6,6 @@ const bookRouter = express.Router();
 bookRouter.get('/', bookController.getAll);
 bookRouter.get('/:id', bookController.getById);
 bookRouter.post('/', bookController.create);
+bookRouter.put('/:id', bookController.update);
 
 module.exports = bookRouter;
