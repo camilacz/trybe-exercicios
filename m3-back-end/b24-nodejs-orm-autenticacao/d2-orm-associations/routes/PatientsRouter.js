4f1f264e13f8e21f0a11376297290cbe5e4418da
camila <camilaczeg@gmail.com>
2022-06-06 18:40:54 -0300

    feat: add route GET /patients/plans/:id

--- m3-back-end/b24-nodejs-orm-autenticacao/d2-orm-associations/routes/PatientsRouter.js
+++ m3-back-end/b24-nodejs-orm-autenticacao/d2-orm-associations/routes/PatientsRouter.js
@@ -4,6 +4,7 @@ const PatientsController = require('../controllers/PatientsController');
 const router = express.Router();
 
 router.get('/plans', PatientsController.getAllPatientsPlans);
+router.get('/plans/:id', PatientsController.getPatientsByPlan);
 router.get('/surgeries', PatientsController.getAllPatientsSurgeries);
 
 module.exports = router;
