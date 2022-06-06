65cdb9101e3e59f0ee2257d087ba66c809ea8711
camila <camilaczeg@gmail.com>
2022-06-06 18:30:32 -0300

    feat: add middleware to get patients and their surgeries

--- m3-back-end/b24-nodejs-orm-autenticacao/d2-orm-associations/controllers/PatientsController.js
+++ m3-back-end/b24-nodejs-orm-autenticacao/d2-orm-associations/controllers/PatientsController.js
@@ -1,11 +1,23 @@
 const PatientsService = require('../services/PatientsService');
 
-const getAllPatientsPlans = async (req, res) => {
+const getAllPatientsPlans = async (_req, res) => {
   const results = await PatientsService.getAllPatientsPlans();
 
   return res.status(200).json(results);
 };
 
+const getAllPatientsSurgeries = async (_req, res) => {
+  try {
+    const results = await PatientsService.getAllPatientsSurgeries();
+
+    return res.status(200).json(results);
+  } catch (err) {
+    console.log(err.message);
+    return res.status(500).json({ message: 'Falha no engano' })
+  }
+};
+
 module.exports = {
   getAllPatientsPlans,
+  getAllPatientsSurgeries,
 }
